2fe9c9679e6ee020cb41e497f0adac7c5a6e84f6
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-17 16:32:35 -0300

    Account login service

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AccountService } from '../shared/account.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  login = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private accountService: AccountService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  async onSubmit() {
+    try {
+      const result = await this.accountService.login(this.login);
+      console.log(`login efetuado: ${result}`);
+      this.router.navigate(['']);
+    } catch (error) {
+      console.log(error);
+    }
   }
-
 }
