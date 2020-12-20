025854039e5972b6195a3c5691c66555dd3cb05b
João Paulo Pereira de Medeiros <medeiros.p@academico.ifrn.edu.br>
2020-12-20 02:00:37 -0300

    feat: mock e-mail

--- server/Services/EmailService.cs
+++ server/Services/EmailService.cs
@@ -1,8 +1,9 @@
 using System;
+using App.Contracts.Services;
 
 namespace App.Services
 {
-    public class EmailService
+    public class EmailService : IEmailService
     {
         public string Send (string destination)
         {
